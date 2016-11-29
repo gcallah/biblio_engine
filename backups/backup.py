7d07d98924bd27b0a9afaa180ab86152253a93d1
gcallah <gcallah@mac.com>
2016-11-29 14:12:28 -0500

    Working on backup script.

--- backups/backup.py
+++ backups/backup.py
@@ -8,4 +8,27 @@ Functions:
 
 """
 
-print("Hello wolrd!")
+import csv
+import django.db
+
+persons = [
+              ['Maudud e Rabi', 'Syed', 'Student', 'Computer Science'],
+              ['bin Hoq', 'Adnan', 'Student', 'Computer Science'],
+              ['Callahan', 'Eugene', 'Faculty', 'Computer Science'],
+          ]
+
+
+def write_records(filenm, data):
+    """
+        Args:
+            filenm: where to output the CSV
+            data: what to output 
+        Returns:
+            None (for now: we probably want success or error codes)
+    """
+    with open(filenm, "w") as f_out:
+        fwriter = csv.writer(f_out)
+        for record in data:
+            fwriter.writerow(record)
+
+
