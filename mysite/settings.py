4f24eee37542b4a6a63359c23ec0547a88d69a3f
gcallah <gcallah@mac.com>
2018-01-21 13:08:08 -0500

    Changing static dir.

--- mysite/settings.py
+++ mysite/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-STATIC_URL = '/mysite/static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     '/berkeley/',
     )
