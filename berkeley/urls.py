fb41bd37fdd1b8e51e5e9217504c56721f7a2757
gcallah <gcallah@mac.com>
2020-06-01 15:46:08 -0400

    Cleaned up URLs.

--- berkeley/urls.py
+++ berkeley/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import url
+from django.urls import re_path
 
 from . import views
 
@@ -6,19 +6,19 @@ app_name = 'berkeley'
 
 
 urlpatterns = [
-    url(r'^$', views.search, name='search'),
-    url(r'^about/*$', views.about, name='about'),
-    url(r'^feedback/*$', views.feedback, name='feedback'),
-    url(r'^publications/*$', views.publications, name='publications'),
-    url(r'^export/(?P<pub_id>[0-9]+)/$', views.export, name='export'),
-    url(r'^pub_detail/(?P<pub_id>[0-9]+)/$', 
-        views.pub_detail, name='pub_detail'),
-    url(r'^edit_pub/(?P<pub_id>[0-9]+)/$', 
-        views.edit_pub, name='edit_pub'),
-    url(r'^person_detail/(?P<person_id>[0-9]+)/$', 
-        views.person_detail, name='person_detail'),
-    url(r'^journal_detail/(?P<journal_id>[0-9]+)/$', 
-        views.journal_detail, name='journal_detail'),
-    url(r'^publisher_detail/(?P<publisher_id>[0-9]+)/$', 
-        views.publisher_detail, name='publisher_detail'),
+    re_path(r'^$', views.search, name='search'),
+    re_path(r'^about/*$', views.about, name='about'),
+    re_path(r'^feedback/*$', views.feedback, name='feedback'),
+    re_path(r'^publications/*$', views.publications, name='publications'),
+    re_path(r'^export/(?P<pub_id>[0-9]+)/$', views.export, name='export'),
+    re_path(r'^pub_detail/(?P<pub_id>[0-9]+)/$', 
+            views.pub_detail, name='pub_detail'),
+    re_path(r'^edit_pub/(?P<pub_id>[0-9]+)/$', 
+            views.edit_pub, name='edit_pub'),
+    re_path(r'^person_detail/(?P<person_id>[0-9]+)/$', 
+            views.person_detail, name='person_detail'),
+    re_path(r'^journal_detail/(?P<journal_id>[0-9]+)/$', 
+            views.journal_detail, name='journal_detail'),
+    re_path(r'^publisher_detail/(?P<publisher_id>[0-9]+)/$', 
+            views.publisher_detail, name='publisher_detail'),
 ]
