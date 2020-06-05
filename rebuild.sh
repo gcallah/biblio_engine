2cff92eecb55624f1f9ccafa08ed7257a6083efb
wm1065 <wm1065@nyu.edu>
2020-06-05 14:41:15 -0400

    Added virtual_env name to rebuild.sh

--- rebuild.sh
+++ rebuild.sh
@@ -4,12 +4,13 @@
 
 export user_name=gcallah
 export project_name=mysite
+export virtual_env=biblio
 export WSGI_configuration_file=/var/www/gcallah_pythonanywhere_com_wsgi.py
 
 # get new source code onto the server
 git pull origin master
 # activate our virtual env:
-source /home/$user_name/.virtualenvs/$project_name/bin/activate
+source /home/$user_name/.virtualenvs/$virtual_env/bin/activate
 # install all of our packages:
 pip install -r requirements/requirements.txt
 echo "Going to reboot the webserver"
